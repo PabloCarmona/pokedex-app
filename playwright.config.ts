4e8d678e6399d31fc6cebc6c031974e03688cc3f
Pablo Carmona <pablo.carmona.gonzalez@ibm.com>
2022-12-19 15:34:59 +0100

    fix: change playwright config and workflows

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,11 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     baseURL: 'http://localhost:3000',
   },
