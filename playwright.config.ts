96723f616278b8857b49f4c4ad9e912a06009454
Pablo Carmona <pablo.carmona.gonzalez@ibm.com>
2022-12-20 14:14:23 +0100

    chore: playwright changes to test timeout errors on ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,12 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:3000',
-    timeout: 180 * 1000,
+    port: 3000,
+    timeout: 60 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3000/',
   },
 }
 
--- queries/getPokemons.ts
+++ queries/getPokemons.ts
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+const API_URL = 'https://q-exercise-api.o64ixruq9hj.us-south.codeengine.appdomain.cloud/api/rest'
+
 export default async function getPokemons() {
   try {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/pokemon/`)
+    const response = await axios.get(`${API_URL}/pokemon/`)
     return response.data
   } catch (error) {
     console.error(error)
