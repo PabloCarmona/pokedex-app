a0b28eae4db7252cfb5acfd5ba647078cc9e22b0
Pablo Carmona <pablo.carmona.gonzalez@ibm.com>
2022-12-20 20:13:47 +0100

    feat(index.tsx): add infinite scroll requests to show all pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,77 @@
+import React from 'react'
+import axios from 'axios'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { useQuery } from '@tanstack/react-query'
-import getPokemons from '../queries/getPokemons'
 import Card from '../components/Card'
 import type { Pokemon } from '../types'
+import styles from '../styles/Home.module.css'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { useInView } from 'react-intersection-observer'
+
+const API_URL = 'https://q-exercise-api.o64ixruq9hj.us-south.codeengine.appdomain.cloud/api/rest'
+
 
 export default function Home() {
-  const { data } = useQuery({ queryKey: ['pokemons'], queryFn: getPokemons })
-  console.log(data)
+  const { ref, inView } = useInView()
+  const fetchPokemons = async ({ pageParam = 0 }) => {
+    try {
+      const res = await axios.get(`${API_URL}/pokemon?offset=${pageParam}`)
+      return res.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-  const renderList = () => data?.items.map((item: Pokemon) => <li key={item.id}><Card {...item} /></li>)
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status
+  } = useInfiniteQuery({
+    queryKey: ['pokemons'],
+    queryFn: fetchPokemons,
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.items.length && pages.length) {
+        return lastPage.offset + 10
+      }
+      return undefined
+    },
+  })
+
+
+  const renderList = () => {
+    if (status === 'loading') {
+      return (<div className={styles.loading}>Loading pokedex...</div>)
+    }
+    if (status === 'error') {
+      console.log({ error })
+      return (<div className={styles.error}>There was an error</div>)
+    }
+
+    return (
+      <ul className={styles.list}>
+        {
+          data.pages.map(page => (
+            <React.Fragment key={crypto.randomUUID()}>
+              {page.items.map((item: Pokemon) => (
+                <li key={item.id}><Card {...item} /></li>
+              ))}
+            </React.Fragment>
+          ))
+        }
+        <li ref={ref}>Last element on the list</li>
+      </ul>
+    )
+  }
+
+  React.useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage({ cancelRefetch: true })
+    }
+  }, [inView])
+
   return (
     <>
       <Head>
@@ -20,9 +82,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <ul className={styles.list}>
-          {renderList()}
-        </ul>
+        {renderList()}
       </main>
     </>
   )
